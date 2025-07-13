efa74c06684b7a73889dc1138e5cdc41ab938ee9
alimoradi296 <moradikor296@gmail.com>
2025-07-13 13:54:14 +0330

    initial front end

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "standalone",
+    experimental: {
+      outputFileTracingRoot: undefined,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
+  
